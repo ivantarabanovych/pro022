5618cf87fdfc45ba6828fd7c83586237b5cfab0d
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-22 14:05:51 +0300

    q3

--- main.js
+++ main.js
@@ -1,52 +1,14 @@
-const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-    
-        let circle = {
-            x: 250,
-            y: 250,
-            radius: 50,
-            color: 'blue',
-            isDragging: false
-        };
+function initMap(){
+    const kyiv = {lat: 50.4501, lng: 30.5234};
 
-        function drawCircle() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.beginPath();
-            ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-            ctx.fillStyle = circle.color;
-            ctx.fill();
-            ctx.closePath();
-        }
-    
-        drawCircle();
+    const map = new goole.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: kyiv
+    });
 
-        function isMouseInCircle(mouseX, mouseY) {
-            const distX = mouseX - circle.x;
-            const distY = mouseY - circle.y;
-            return Math.sqrt(distX * distY + distY * distY) < circle.radius;
-        }
-
-        canvas.addEventListener('mousedown', (e) => {
-            const mouseX = e.offsetX;
-            const mouseY = e.offsetY;
-    
-            if (isMouseInCircle(mouseX, mouseY)) {
-                circle.isDragging = true;
-                circle.color = 'red'; 
-                drawCircle();
-            }
-        });
-
-        canvas.addEventListener('mousemove', (e) => {
-            if (circle.isDragging) {
-                circle.x = e.offsetX;
-                circle.y = e.offsetY;
-                drawCircle();
-            }
-        });
-
-        canvas.addEventListener('mouseup', () => {
-            circle.isDragging = false;
-            circle.color = 'blue';
-            drawCircle();
-        });
+    const marker = new goole.maps.Marker({
+        position: kyiv,
+        map: map,
+        title: 'Київ'
+    });
+}

866576e2b427c60bac51f3b41811e508e3d4d712
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-22 14:21:30 +0300

    q4

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
+let map, marker;
+
 function initMap(){
     const kyiv = {lat: 50.4501, lng: 30.5234};
 
-    const map = new goole.maps.Map(document.getElementById('map'), {
+    map = new goole.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: kyiv
     });
 
-    const marker = new goole.maps.Marker({
+    marker = new goole.maps.Marker({
         position: kyiv,
         map: map,
-        title: 'Київ'
+        title: 'Ваша локація'
     });
+
+    getUserLocation();
+}
+
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const userPosition = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                document.getElementById('lat').textContent = userPosition.lat.toFixed(5);
+                document.getElementById('lng').textContent = userPosition.lng.toFixed(5);
+
+                marker.setPosition(userPosition);
+
+                map.setCenter(userPosition);
+            },
+            error => {
+                alert("Не вдалося отримати ваше місцеперебування. Перевірте налаштування доступу до геолокації.");
+            }
+        );
+    } else {
+        alert("Ваш браузер не підтримує Geolocation API.");
+    }
 }
